22087bea75e6422394c1e3b7c8d676e3a2571c1b
Tomaz Toplak <tomaz@arabsu.com>
2017-02-21 20:58:49 +0100

    Create a simple gray circle in the container.

--- js/circular-slider.js
+++ js/circular-slider.js
@@ -1,5 +1,14 @@
 var makeCircularSlider = function(args) {
+    
+    //we have to know which container to use to draw the slider
+    if (args.container === undefined)
+        return {};
         
+    //our container
+    var container = document.getElementById(args.container);
+    
+    var sliderCount = 0;
+    
     var defaultOptions = {
         color: "#ff3366", //the celtra logo color is set if no color is defined ;)
         maxValue: 255,
@@ -15,12 +24,31 @@ var makeCircularSlider = function(args) {
     }        
     
     var CircularSlider = function(args) {
+        var sliderCircle;  
+        var sliderCenter;        
         
         (function() {
             //constructor
+            initSliderVisuals();
         })();
         
         //private stuff
+        function initSliderVisuals() { 
+            //main circle of the slider
+            sliderCircle = document.createElement('div');
+            sliderCircle.setAttribute("id", "sliderCircle" + sliderCount);
+            container.appendChild(sliderCircle);            
+            sliderCircle.style.width = 2 * args.radius + "px";
+            sliderCircle.style.height = 2 * args.radius + "px";
+            sliderCircle.style.background = "#cfcfd0";
+            
+            //masking circle... so it covers the center and we only see the edge of the main circle
+            sliderCenter = document.createElement('div');
+            sliderCenter.setAttribute("id", "sliderCenter" + sliderCount);
+            container.appendChild(sliderCenter);            
+            sliderCenter.style.width = (2 * args.radius - 30) + "px";
+            sliderCenter.style.height = (2 * args.radius - 30) + "px";
+        }
         
         //public stuff
         return {
